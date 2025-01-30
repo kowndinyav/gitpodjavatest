f4a06fda5a14eca77880871b4a8109c8f6038abb
Kowndinya V <kowndiny@adobe.com>
2025-01-30 22:30:15 +0530

    updating tomcat path

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Download and setup Tomcat
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+
 tar xvf apache-tomcat-9.0.85.tar.gz
 rm apache-tomcat-9.0.85.tar.gz
 mv apache-tomcat-9.0.85 ~/tomcat
